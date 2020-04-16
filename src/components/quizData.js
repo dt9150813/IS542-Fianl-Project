80908c73024e426c0316b98a6baef39745102bda
Eddy Hsu <eddy0712@gmail.com>
2020-04-15 19:05:59 -0600

    first commit

---
+++ src/components/quizData.js
@@ -0,0 +1,33 @@
+export const quizData = [
+    {
+      id: 0,
+      question: ` Do you have Fever ?`,
+      options: [`Yes`,  `No`],
+      answer: `Yes`
+    },
+    {
+      id: 1,
+      question: `Do you dry Cough?`,
+      options: [`Yes`,  `No`],
+      answer: `Yes`
+    },
+    {
+      id: 3,
+      question: `Do you have Shortness of breath?`,
+      options: [`Yes`,  `No`],
+      answer: `Yes`
+    },
+    {
+        id: 4,
+        question: `Do you have difficulty breathing?`,
+        options: [`Yes`,  `No`],
+        answer: `Yes`
+      },
+    {
+      id: 5,
+      question: `Do you have sore throat?`,
+      options: [`Yes`,  `No`],
+      answer: `Yes`
+    }
+  ];
+  
